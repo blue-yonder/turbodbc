cd188745608b6f5b9003a7016ea9ceb5c6398b9c
yaxxie <yaxxie@users.noreply.github.com>
2017-03-24 09:40:52 +0000

    forgot to clean up the unused stuff

--- cpp/turbodbc_numpy/Library/src/datetime_column.cpp
+++ cpp/turbodbc_numpy/Library/src/datetime_column.cpp
@@ -6,9 +6,6 @@
 
 #include <Python.h>
 
-#include <boost/date_time/gregorian/gregorian_types.hpp>
-#include <boost/date_time/posix_time/posix_time_types.hpp>
-
 #include <sql.h>
 #include <cstring>
 
@@ -23,26 +20,6 @@ namespace {
 		} else {
 			return "datetime64[D]";
 		}
-	}
-
-	boost::posix_time::ptime const timestamp_epoch({1970, 1, 1}, {0, 0, 0, 0});
-
-	long timestamp_to_microseconds(char const * data_pointer)
-	{
-		auto & sql_ts = *reinterpret_cast<SQL_TIMESTAMP_STRUCT const *>(data_pointer);
-		long const microseconds = sql_ts.fraction / 1000;
-		boost::posix_time::ptime const ts({static_cast<unsigned short>(sql_ts.year), sql_ts.month, sql_ts.day},
-		                                  {sql_ts.hour, sql_ts.minute, sql_ts.second, microseconds});
-		return (ts - timestamp_epoch).total_microseconds();
-	}
-
-	boost::gregorian::date const date_epoch(1970, 1, 1);
-
-	long date_to_days(char const * data_pointer)
-	{
-		auto & sql_date = *reinterpret_cast<SQL_DATE_STRUCT const *>(data_pointer);
-		boost::gregorian::date const date(sql_date.year, sql_date.month, sql_date.day);
-		return (date - date_epoch).days();
 	}
 
 	datetime_column::converter make_converter(turbodbc::type_code type)
