713ce4f008b5d0bfd8d519746f14b300086e05b8
Jochen Ott <Jochen.Ott@blue-yonder.com>
2019-02-25 18:42:21 +0100

    modernize contrib/docker setup
     * use official docker images for mssql, mysql, postgres
     * use docker-compose to keep db services separate from build image,
       so one can e.g. use the db services also for building natively

--- contrib/docker/build_and_test.sh
+++ contrib/docker/build_and_test.sh
@@ -1,3 +1,17 @@
-#/bin/bash
+#!/bin/bash
 
-docker run --rm -t -i -v $PWD:/io turbodbc_base /io/contrib/docker/build_and_test_in_docker.sh
+# This should run in the 'turbodbc' docker service
+
+set -ex
+
+mkdir -p /turbodbc/build_docker && cd /turbodbc/build_docker
+cmake -DCMAKE_INSTALL_PREFIX=./dist -DPYBIND11_PYTHON_VERSION=3.5 ..
+make -j4
+
+
+# run all tests (C++ and python tests):
+ctest --verbose
+
+# to just run the python integration tests use these commands:
+# cd /turbodbc/python/turbodbc_test
+# pytest
