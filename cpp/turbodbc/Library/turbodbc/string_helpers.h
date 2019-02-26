b033dca4498e32f56a585373660d696495f6ef32
Jochen Ott <Jochen.Ott@blue-yonder.com>
2019-02-26 18:03:02 +0100

    Improve docs for string_helpers:buffered_string_size

--- cpp/turbodbc/Library/turbodbc/string_helpers.h
+++ cpp/turbodbc/Library/turbodbc/string_helpers.h
@@ -3,6 +3,12 @@
 
 namespace turbodbc {
 
+/***
+ * @brief Determine the number of bytes returned by odbc for string types, given a length indicator
+ * @param length_indicator The length indicator, as returned by odbc. Must not be SQL_NULL_DATA, but may be SQL_NO_TOTAL
+ * @param maximum_string_size The maximum string size in bytes, used if length_indicator is SQL_NO_TOTAL;
+ *        should be set to buffer size in bytes minus the byte-size of the null termination character
+ */
 std::size_t buffered_string_size(intptr_t length_indicator, std::size_t maximum_string_size);
 
 }
