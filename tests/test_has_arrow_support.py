cae3a442de5b3e748d468db411633dd8176c32fe
Uwe L. Korn <uwe.korn@quantco.com>
2024-08-27 13:39:59 +0200

    Run pytest in CI

--- tests/test_has_arrow_support.py
+++ tests/test_has_arrow_support.py
@@ -1,22 +1,12 @@
 from unittest.mock import patch
 
-import pytest
-
 from turbodbc.cursor import _has_arrow_support
 
-# Skip all parquet tests if we can't import pyarrow.parquet
-pytest.importorskip("pyarrow")
 
-# Ignore these with pytest ... -m 'not parquet'
-pyarrow = pytest.mark.pyarrow
-
-
-@pyarrow
 def test_has_arrow_support_fails():
     with patch("builtins.__import__", side_effect=ImportError):
         assert not _has_arrow_support()
 
 
-@pyarrow
 def test_has_arrow_support_succeeds():
     assert _has_arrow_support()
